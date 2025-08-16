6c4fee8551501d463cd40d82873d3c2e671b8eff
Akshat <akshatvijayvergiya64@gmail.com>
2025-08-16 22:56:37 +0530

    Constant changed

--- common/services/Constant.js
+++ common/services/Constant.js
@@ -1,27 +1,28 @@
 module.exports = {
-    AUTH_TYPE: {
-      GOOGLE: "google",
-      DIRECT: "direct",
-    },
-    ROLE: {
-      USER: "user",
-      ADMIN: "admin",
-    },
-    PROFILE_TYPE: {
-      PRIVATE: "private",
-      PUBLIC: "public",
-    },
-    SUCCESS: 200,
-    BAD_REQUEST: 400,
-    PAGE_NOT_FOUND: 404,
-    UNAUTHORIZED: 401,
-    INTERNAL_SERVER: 500,
-    NOT_ACCEPTABLE: 406,
-    DATA_CONFLICT: 409,
-    CREATED: 201,
-    FAIL: 400,
-    ACTIVE: "1",
-    INACTIVE: "0",
-    DELETE: "2",
-  };
-  
+  AUTH_TYPE: {
+    GOOGLE: "google",
+    DIRECT: "direct",
+  },
+  ROLE: {
+    USER: "user",
+    ADMIN: "admin",
+  },
+  PROFILE_TYPE: {
+    PRIVATE: "private",
+    PUBLIC: "public",
+  },
+  SUCCESS: 200,
+  BAD_REQUEST: 400,
+  PAGE_NOT_FOUND: 404,
+  UNAUTHORIZED: 401,
+  INTERNAL_SERVER: 500,
+  NOT_ACCEPTABLE: 406,
+  DATA_CONFLICT: 409,
+  CREATED: 201,
+  FAIL: 400,
+  ACTIVE: "1",
+  INACTIVE: "0",
+  DELETE: "2",
+  TRUE: true,
+  FALSE: false,
+};
